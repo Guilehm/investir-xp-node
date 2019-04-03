dd7ddcc5bfb5458e154e42d12e77fa96719defb4
Guilherme Luis <guile.hm@hotmail.com>
2019-04-02 23:10:35 -0300

    refactor userId

--- controllers/getUser.js
+++ controllers/getUser.js
@@ -1,10 +1,10 @@
 const https = require('../services/https')
 
 module.exports = (req, res) => {
-    let userId = req.params.user_id
+    let userId = req.params.userId
     if (!userId) {
         res.status(400).json({
-            error: 'Param user is required'
+            error: 'user id is required'
         })
     }
 

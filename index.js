e348f5411fdf978b6dbea5a2706b88f3d49175ca
Guilherme Luis <guile.hm@hotmail.com>
2019-04-02 22:00:41 -0300

    add controller to index

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = new express();
+const app = new express()
 
-app.use(express.static('public'));
+const getPlayerController = require('./controllers/getPlayer')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    console.log('chegou aqui')
-    console.log(path.resolve(__dirname, ))
     res.sendFile(path.resolve(__dirname, 'views/layouts/base/base.html'))
 })
 
+app.get('/players', getPlayerController)
+
 app.listen(4000, () => {
-    console.log('App listening on port 4000...');
+    console.log('App listening on port 4000...')
 })

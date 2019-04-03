5d2bd151ca0e25a3a7df0f69065e263156ff3201
Guilherme Luis <guile.hm@hotmail.com>
2019-04-02 22:16:41 -0300

    create https service

---
+++ services/https.js
@@ -0,0 +1,36 @@
+const https = require('https');
+
+const CONNECTION_TIMEOUT = 1000
+
+function getUrlData(url = 'https://jsonplaceholder.typicode.com/users') {
+
+    return new Promise((resolve, reject) => {
+        let dealWithResponse = response => {
+
+            let data = ''
+            response.on('data', dataChunk => data += dataChunk)
+            response.on('end', () => {
+                try {
+                    let obj = JSON.parse(data)
+                    resolve(obj)
+                } catch (e) {
+                    reject(e)
+                }
+            })
+
+        }
+
+        let request = https
+            .get(url, dealWithResponse)
+            .on('error', (e) => {
+                reject(e)
+            })
+
+        request.setTimeout(CONNECTION_TIMEOUT, () => {
+            request.abort()
+        })
+        request.end()
+    })
+}
+
+module.exports = getUrlData

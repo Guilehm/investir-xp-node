1803a2897f43b8e33dcd872239b29b5a45d9b3a2
Guilherme Luis <guile.hm@hotmail.com>
2019-04-02 22:16:56 -0300

    improve get player controller

--- controllers/getPlayer.js
+++ controllers/getPlayer.js
@@ -1,3 +1,5 @@
+const https = require('../services/https')
+
 module.exports = (req, res) => {
     let userId = req.query.user_id
     if (!userId) {
@@ -7,9 +9,20 @@ module.exports = (req, res) => {
     }
 
     let endpoint = `https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats_v2?user_id=${userId}`
+
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({
-        user: userId,
-        url: endpoint,
-    }))
+
+    let handleSuccess = data => {
+        res.end(JSON.stringify({
+            data
+        }))
+    }
+
+    let handleFailure = error => {
+        res.end(JSON.stringify({
+            error: error,
+        }))
+    }
+
+    https(endpoint).then(handleSuccess, handleFailure)
 }

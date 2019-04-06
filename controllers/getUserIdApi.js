ce560b58f83cb144b6990808310e65bc76fd743f
Guilherme Luis <guile.hm@hotmail.com>
2019-04-06 12:10:49 -0300

    refactor getUserIdApi

--- controllers/getUserIdApi.js
+++ controllers/getUserIdApi.js
@@ -1,16 +1,19 @@
 const https = require('../services/https')
 
 module.exports = (req, res) => {
-    let username = req.query.username
+
+    res.setHeader('Content-Type', 'application/json')
+
+    let username = req.params.username
     if (!username) {
         res.status(400).json({
-            error: 'user id is required'
+            error: 'username is required'
         })
     }
 
     let endpoint = `https://fortnite-public-api.theapinetwork.com/prod09/users/id?username=${username}`
 
-    res.setHeader('Content-Type', 'application/json')
+    https(endpoint).then(handleSuccess, handleFailure)
 
     let handleSuccess = data => {
         res.end(JSON.stringify({
@@ -24,5 +27,4 @@ module.exports = (req, res) => {
         }))
     }
 
-    https(endpoint).then(handleSuccess, handleFailure)
 }

f456037f08322ab5c3ecb5adf8b146d0c060371f
Guilherme Luis <guile.hm@hotmail.com>
2019-04-06 12:22:21 -0300

    create user stats controller

---
+++ controllers/getUserStatsApi.js
@@ -0,0 +1,25 @@
+const https = require('../services/https')
+
+module.exports = (req, res) => {
+
+    res.setHeader('Content-Type', 'application/json')
+
+    let userId = req.params.userId
+
+    let handleSuccess = data => {
+        res.end(JSON.stringify({
+            data
+        }))
+    }
+
+    let handleFailure = error => {
+        res.end(JSON.stringify({
+            error: error,
+        }))
+    }
+
+    let endpoint = `https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats_v2?user_id=${userId}`
+
+    https(endpoint).then(handleSuccess, handleFailure)
+
+}

fdfd9816f8a514d4b7fbc494432787a24f1334be
Guilherme <guile.hm@hotmail.com>
2019-04-06 20:11:03 -0300

    fix bug

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ VIEWS_DIR = './views/'
 
 const app = new express()
 
-const mongoConfig = require('./config/mongo')
 const env = process.env.NODE_ENV || 'development'
-const envUrl = process.env[config.use_env_variable]
+const mongoConfig = require('./config/mongo')[env]
+const envUrl = process.env[mongoConfig.use_env_variable]
 const DevUrl = `mongodb://${mongoConfig.host}/${mongoConfig.database}`
 const mongoUrl = envUrl ? envUrl : DevUrl
 

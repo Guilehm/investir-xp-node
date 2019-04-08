d5088e10fba9451cf8054638c7f795ef647c38b8
Guilherme <guile.hm@hotmail.com>
2019-04-08 18:17:01 -0300

    create createFriend controller

---
+++ controllers/api/createFriendApi.js
@@ -0,0 +1,26 @@
+const Friend = require('../../database/models/Friend')
+
+module.exports = async (req, res) => {
+    let accountId = req.params.accountId
+    let mainMode = req.body.mainMode
+
+    let handleError = err => {
+        res.status(400).end(JSON.stringify({
+            err
+        }))
+    }
+
+    let handleSuccess = data => {
+        if (!data.lastErrorObject.updatedExisting) {
+            res.status(201)
+        }
+        res.end(JSON.stringify({
+            data
+        }))
+    }
+
+    await Friend.findOneAndUpdate({ accountId },
+        { accountId, mainMode }, {
+            upsert: true, new: true, runValidators: true, rawResult: true
+        }).then(handleSuccess, handleError)
+}

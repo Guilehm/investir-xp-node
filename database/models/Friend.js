ec8ccb83ac913952b09d993d5acfdc5750fcdb45
Guilherme <guile.hm@hotmail.com>
2019-04-08 16:53:15 -0300

    create friend schema

---
+++ database/models/Friend.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const FriendSchema = new mongoose.Schema({
+    accountId: {
+        type: String,
+        required: true,
+    },
+    mainMode: {
+        type: String,
+        required: true,
+        enum: ['keyboardmouse', 'gamepad']
+    },
+})
+
+const Friend = mongoose.model('Friend', FriendSchema)
+
+module.exports = Friend

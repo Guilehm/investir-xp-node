face537848cbcd9db21df5596e09ba422e27db58
Guilherme Luis <guile.hm@hotmail.com>
2019-04-10 21:51:41 -0300

    refactor

--- controllers/auth/createUserController.js
+++ controllers/auth/createUserController.js
@@ -1,11 +1,3 @@
-const User = require('../../database/models/auth/User')
-
 module.exports = (req, res) => {
-    User.create(req.body, (err, user) => {
-        if (err) {
-            console.log(err)
-            res.redirect('/auth/register/')
-        }
-        res.redirect('/')
-    })
+    res.render('layouts/user-create')
 }

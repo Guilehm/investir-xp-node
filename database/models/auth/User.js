71eca6602c52af743278681155b6f6f74826cfde
Guilherme Luis <guile.hm@hotmail.com>
2019-04-10 21:40:09 -0300

    create UserSchema

---
+++ database/models/auth/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+UserSchema.pre('save', function(next) {
+    const user = this;
+    bcrypt.hash(user.password, 10, (error, encrypted) => {
+        user.password = encrypted;
+        next()
+    })
+})
